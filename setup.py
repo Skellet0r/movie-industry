dd9d69cf11a865f47084a50a127a4565799a318b
Edward Amor <edward.amor3@gmail.com>
2020-06-02 15:44:32 -0400

    feat: add setup.py allowing import or src module
    Adding this setup.py allows the user to use the scripts
    tools in the src directory by installing it as a python
    package. pip install -e .

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="src",
+    version="",
+    author="",
+    author_email="",
+    description="",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+)
